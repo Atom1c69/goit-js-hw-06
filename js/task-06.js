cfbf8e75a3b0bdac6ceb31c90910061022b1f00c
Atom1c69 <atomicc555@icloud.com>
2023-09-07 15:06:02 +0300

    correction.task №6

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,6 @@
 function setValidationClasses(input, isValid) {
-  if (isValid) {
-    input.classList.remove("invalid");
-    input.classList.add("valid");
-  } else {
-    input.classList.remove("valid");
-    input.classList.add("invalid");
-  }
+  input.classList.toggle("valid", isValid);
+  input.classList.toggle("invalid", !isValid);
 }
 
 const input = document.getElementById("validation-input");
